6a35ba46b5aa22f76a3a95830f61beea41231135
Jeremy <asirwaadj@gmail.com>
2022-01-12 01:57:35 +0530

    made popup title responsive

--- src/components/LoginPage/Login.js
+++ src/components/LoginPage/Login.js
@@ -54,40 +54,15 @@ function Login() {
 
     return (
         <div className='loginpage'>
-            {/* <FiMail />   
-            <FiLock /> */}
-            <div className="logincontainer">
-                <div className="loginimgcontainer">
+            {/* <div className="logincontainer">
+                    <div className="loginimgdiv">
 
-                </div>
-                <div className="logininfo">
-                    <div className="logintext">
-                        <h1><span className='weare'>We Are</span><span className='aazons' style={{"color" : "#0916c8"}}> Aaronz Connect</span></h1>
-                        <p>Welcome back! Log in to your account <br /> to view today's activities:</p>
                     </div>
-                    <div className="logincreds">
-                        <p className='errormsg'>{errormsg } </p>
-                        <div className="emaildiv">
-                            <FiMail /> 
-                             <input className='email' name = "email" onChange={ onchangehandler } type="text" placeholder='Email' />
-                        </div>
-                        <div className="passworddiv">
-                             <FiLock />  
-                            <input className='password' name = "password" onChange={ onchangehandler } type="password" placeholder='Password' />
-                        </div>
-                        <p className='linkptag'><a href="#">Forgot Password ?</a></p>
-                        <button className='loginbtn' onClick={ () => { login(); } }>Login</button>
+                    <div className="logincreddiv">
+
                     </div>
-                    <h3>Or</h3>
-                    <a href="#" class="google btn">
-                        <i class="fab fa-google"></i>  Login with Google
-                    </a>
-                    <a href="#" class="fb btn">
-                        <i class="fab fa-facebook-f"></i>  Login with Facebook
-                    </a>
-                    <p style={{"position" : "relative", "top" : "20px", "fontSize" : "12px"}}>Not a user <Link to = "/register">Signup</Link> </p>
-                </div>
-            </div>
+            </div> */}
+            <span>hello</span>
         </div>
     )
 }
